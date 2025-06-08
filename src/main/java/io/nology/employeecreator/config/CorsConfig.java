2ae8a7663cfa9962f67241bb2efe22d03667ac03
FJack <fredlishiajack@gmail.com>
2025-06-08 10:57:45 +1000

    add global cors configuration

---
+++ src/main/java/io/nology/employeecreator/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package io.nology.employeecreator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    /*
+    Allows all endpoints
+    Accepts frontend URL
+     */
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("POST", "GET", "PATCH", "DELETE").allowedHeaders("*");
+    }
+}
