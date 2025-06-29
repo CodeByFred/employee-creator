df8299df4dfc8ac7408951e29c143deafcedc078
FJack <fredlishiajack@gmail.com>
2025-06-29 12:37:08 +1000

    fix base link for all page redirects

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <main>
         <Routes>
           <Route path="/" element={<Navigate to="/employees" />} />
